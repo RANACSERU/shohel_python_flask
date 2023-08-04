08845f7c19be264776c39099697037f9bf85e740
RANACSERU <ranacseru37@gmail.com>
2023-08-04 11:02:14 +0000

    jobtitile.html is created and added to home.html

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Dhaka, Bangladesh',
+  'salary': '1000000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Rajshahi, Bangladesh',
+  'salary': '1500000'
+}, {
+  'id': 3,
+  'title': 'Front End Developer',
+  'location': 'Remote',
+}, {
+  'id': 4,
+  'title': 'Back End Developer',
+  'location': 'Khulna, Bangladesh',
+  'salary': '1800000'
+}]
+
 
 @app.route("/")
 def hello_shohel():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 # # print(__name__)
